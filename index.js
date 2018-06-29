824179979a940815dd8abc5091fec8da467b0e2b
Sharol Dsouza <sharol21892@gmail.com>
2018-06-29 04:22:07 -0700

    final commit
    changes to data

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 
 var docList = [
-    {name:"Ann Maria",speciality:"General Practice",location:"San Diego",state:"California",phone:"(818)345-7707",rating:"4"},
-    {name:"Joseph Bell",speciality:"Pediatrics",location:"San Francisco",state:"California",phone:"(818) 365-1194",rating:"3.5"},
-    {name:"Noah Terry",speciality:"General Practice",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
-    {name:"Ang Oakley",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
-    {name:"Angela Oakley",speciality:"Dentist",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Ann Maria",speciality:"General Practice",location:"San Diego",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Joseph Bell",speciality:"Pediatrics",location:"San Francisco",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Noah Terry",speciality:"Dermatology",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Ang Oakley",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Angela Oakley",speciality:"Dentist",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Antony Perez",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Joe Bell",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Jon Terry",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Ang Oakley",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Sisu Ryan",speciality:"Dentist",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+{name:"Nitin Gopala",speciality:"General Practice",location:"San Diego",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Treasach Victor",speciality:"Pediatrics",location:"San Francisco",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Behiya Shoba",speciality:"Dermatology",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Eppi Gerfrid",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Mariho Spas",speciality:"Dentist",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Juda Mesi",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Ohana Bell",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Yashiyanu Nero",speciality:"Colon and Rectal",location:"Sacramento",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Godwin Oakley",speciality:"Chiropractic",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Angela Markus",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+{name:"Anisha June",speciality:"Accupuncture",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Julio Alexender",speciality:"Dentist",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Julio Markes",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Joanna Bell",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Aktsuki Nero",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Shakti Patel",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Kusuma Jaiswal",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+{name:"Chrysilla Gerfrid",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Celiene Dsouza",speciality:"Dentist",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Jessica Lisbet",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Rae Kim",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Ryong kim",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Godwin Moe",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Angela Fernandes",speciality:"General Practice",location:"Fresno",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+{name:"Eppi Gerfrid",speciality:"Accupuncture",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Mariho K Lun",speciality:"Dentist",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Juda Pinto",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Ohana Citra",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Yashiyanu Ho",speciality:"Colon and Rectal",location:"Fresno",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Godwin Oakley",speciality:"Chiropractic",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Angela Mark Dias",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+{name:"Eppi Gerfrid",speciality:"Accupuncture",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Mariho Jacobe",speciality:"Dentist",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+  {name:"Judah Doe",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+  {name:"Ohana Bella",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+  {name:"Yashiyanu Yun",speciality:"Colon and Rectal",location:"Fresno",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+  {name:"Godwin Lowa",speciality:"Chiropractic",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+  {name:"Angela Dcruz",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
 
- 
- 
-  ]
+
+
+]
     
     var divList = document.getElementById("myTable");
   // creating all cells
--- viewDoctor.js
+++ viewDoctor.js
@@ -1,12 +1,57 @@
 var docList = [
-    {name:"Ann Maria",img:"img.jpg",speciality:"General Practice",location:"San Diego",state:"California",address:"134370 BURBANK BLVD SUITETARZANA, CA 91356",phone:"(818)345-7707"},
-    {name:"Joseph Bell",img:"img.jpg",speciality:"Pediatrics",location:"San Francisco",state:"California",address:"11550 INDIAN HILLS ROAD, SUITE # 371 MISSION HILLS, CA 91345 ",phone:"(818) 365-1194"},
-    {name:"Noah Terry",img:"img.jpg",speciality:"General Practice",location:"Long Beach",state:"California",address:"27420 TOURNEY ROAD, SUITE # 200 VALENCIA, CA 91355",phone:"(661) 753-9891"},
-    {name:"Ang Oakley",img:"img.jpg",speciality:"Accupuncture",location:"Long Beach ",state:"California",address:"3100 E FLORENCE AVE, SUITE # 1 HUNTINGTON PARK, CA 90255",phone:"(323) 583-4115"},
-    {name:"Angela Oakley",img:"img.jpg",speciality:"Dentist",location:"LongBeach ",state:"California",address:"3100 E FLORENCE AVE, SUITE # 1 HUNTINGTON PARK, CA 90255",phone:"(323) 583-4115"}
+    {name:"Ann Maria",img:"img.jpg",speciality:"General Practice",location:"San Diego",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Joseph Bell",img:"img1.jpg",speciality:"Pediatrics",location:"San Francisco",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Noah Terry",img:"img2.jpg",speciality:"Dermatology",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Ang Oakley",img:"img3.jpg",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Angela Oakley",img:"img4.jpg",speciality:"Dentist",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Antony Perez",img:"img.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Joe Bell",img:"img1.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Jon Terry",img:"img.jpg",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Ang Oakley",img:"img2.jpg",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Sisu Ryan",img:"img4.jpg",speciality:"Dentist",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+	{name:"Nitin Gopala",img:"img.jpg",speciality:"General Practice",location:"San Diego",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Treasach Victor",img:"img2.jpg",speciality:"Pediatrics",location:"San Francisco",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Behiya Shoba",img:"img.jpg",speciality:"Dermatology",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Eppi Gerfrid",img:"img2.jpg",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Mariho Spas",img:"img1.jpg",speciality:"Dentist",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Juda Mesi",img:"img.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Ohana Bell",img:"img2.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Yashiyanu Nero",img:"img3.jpg",speciality:"Colon and Rectal",location:"Sacramento",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Godwin Oakley",img:"img1.jpg",speciality:"Chiropractic",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Angela Markus",img:"img.jpg",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+	{name:"Anisha June",img:"img.jpg",speciality:"Accupuncture",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Julio Alexender",img:"img2.jpg",speciality:"Dentist",location:"Sacramento ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Julio Markes",img:"img1.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Joanna Bell",img:"img4.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Aktsuki Nero",img:"img2.jpg",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Shakti Patel",img:"img1.jpg",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Kusuma Jaiswal",img:"img1.jpg",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+	{name:"Chrysilla Gerfrid",img:"img2.jpg",speciality:"Accupuncture",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Celiene Dsouza",img:"img4.jpg",speciality:"Dentist",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Jessica Lisbet",img:"img4.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Rae Kim",img:"img.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Ryong kim",img:"img4.jpg",speciality:"Colon and Rectal",location:"Long Beach",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Godwin Moe",img:"img.jpg",speciality:"Chiropractic",location:"Long Beach ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Angela Fernandes",img:"img3.jpg",speciality:"General Practice",location:"Fresno",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+	{name:"Eppi Gerfrid",img:"img.jpg",speciality:"Accupuncture",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Mariho K Lun",img:"img2.jpg",speciality:"Dentist",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Juda Pinto",img:"img1.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Ohana Citra",img:"img.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Yashiyanu Ho",img:"img.jpg",speciality:"Colon and Rectal",location:"Fresno",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Godwin Oakley",img:"img3.jpg",speciality:"Chiropractic",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Angela Mark Dias",img:"img4.jpg",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+	{name:"Eppi Gerfrid",img:"img.jpg",speciality:"Accupuncture",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Mariho Jacobe",img:"img2.jpg",speciality:"Dentist",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.5"},
+    {name:"Judah Doe",img:"img1.jpg",speciality:"Allergy and Immunology",location:"Santa Barbara",state:"California",phone:"(818)345-7707",rating:"4"},
+    {name:"Ohana Bella",img:"img4.jpg",speciality:"Cardiology",location:"Los Angeles",state:"California",phone:"(818) 365-1194",rating:"3.5"},
+    {name:"Yashiyanu Yun",img:"img3.jpg",speciality:"Colon and Rectal",location:"Fresno",state:"California",phone:"(661) 753-9891",rating:"2.5"},
+    {name:"Godwin Lowa",img:"img2.jpg",speciality:"Chiropractic",location:"Fresno ",state:"California",phone:"(323) 583-4115",rating:"4.7"},
+    {name:"Angela Dcruz",img:"img1.jpg",speciality:"General Practice",location:"Los Angeles",state:"California",phone:"(323) 583-4115",rating:"4.5"},
 
-]
-var priority = {name:"1",img:"1",speciality:"40",location:"30",state:"20",address:"1",phone:"1"};
+ 
+ 
+  ]
+var priority = {name:"0",img:"0",speciality:"40",location:"20",state:"10",address:"0",phone:"0"};
 var x = document.URL;
 var id = x.split("=")[1];
 var docInfo = document.getElementById("docInfo");
@@ -44,10 +89,13 @@ var rel = []
 for(var a =0 ;a <docList.length;a++){
     if(a!=i){
         var sim = 0;
-        for(var b in docList[a]){
-           if(docList[a][b]==docList[i][b]) sim =sim+parseInt(priority[b]);
-        }
-        rel.push({a,sim});
+       if(docList[a]["speciality"]==docList[i]["speciality"]) sim =sim+parseInt(priority["speciality"]);       
+       if(docList[a]["location"]==docList[i]["location"]) sim =sim+parseInt(priority["location"]);   
+       if(docList[a]["state"]==docList[i]["state"]) sim =sim+parseInt(priority["state"]);   
+        
+        
+       rel.push({a,sim});
+        console.log(a+" "+sim);
     }   
 }
 rel.sort(function(a,b){
